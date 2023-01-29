1a30c63647cbb441679304ecfa9eadab9bd22de5
Abhinav Upreti <abhinav280@gmail.com>
2023-01-29 23:52:53 +0530

    CQRS Latest

---
+++ Hr.LeaveManagement.Application/Features/LeaveType/Command/CreateLeaveType/CreateLeaveTypeCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveType.Command.CreateLeaveType
+{
+    public class CreateLeaveTypeCommand : IRequest<int>
+    {
+    }
+}
---
+++ Hr.LeaveManagement.Application/Features/LeaveType/Command/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Hr.LeaveManagement.Application.Contracts.Persistence;
+using Hr.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveType.Command.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _ILeaveTypeRepository;
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository ILeaveTypeRepository)
+        {
+            _mapper = mapper;
+            _ILeaveTypeRepository = ILeaveTypeRepository;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+            await _ILeaveTypeRepository.CreateAsync(leaveTypeToCreate);
+            return leaveTypeToCreate.Id;
+        }
+    }
+}
